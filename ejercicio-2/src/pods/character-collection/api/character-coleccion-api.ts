d8a4bd76c91071938b6903c66c6decb39847d51c
Katherine Paucar <gaby2091@outlook.es>
2024-03-16 17:19:06 +0100

    Update character-coleccion-api.ts

--- ejercicio-2/src/pods/character-collection/api/character-coleccion-api.ts
+++ ejercicio-2/src/pods/character-collection/api/character-coleccion-api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { CharacterCollection } from './character-collection-api-model';
+import { Character } from './character-collection-api-model';
 // const url = 'https://rickandmortyapi.com/api/character';
 const characterUrl = '/api/charactersCollection';
-export const getCharacterCollection = async (): Promise<CharacterCollection> => {
+export const getCharacterCollection = async (): Promise<Character> => {
   const { data } = await axios.get(characterUrl);
   return data;
 };
